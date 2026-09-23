e533e2e329f8884522486445e216c15d3515efab
VIV\vivian <vivianmail.cc@gmail.com>
2026-09-23 16:56:04 -0700

    set up website and app.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+
 const app = express();
+
 const PORT = 3000;
-app.get("/", (req,res) => {
-    res.send("Test");
-}
-);
 
-app.listen(PORT,() => {
-    console.log(`Server running at http://localhost:${PORT}`)
+app.use(express.static("public"));
 
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
