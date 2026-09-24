4458448e424f9c55f390a5e5520f54f931f688e1
VIV\vivian <vivianmail.cc@gmail.com>
2026-09-23 19:28:00 -0700

    add database connection

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+const { Pool } = require("pg");
 
 const app = express();
 
 const PORT = 3000;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
 
 app.use(express.static("public"));
 app.use(express.json());
