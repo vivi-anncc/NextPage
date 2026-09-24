2596c939882808a3be5ac10dc3e1cfed2fac178e
VIV\vivian <vivianmail.cc@gmail.com>
2026-09-23 18:29:37 -0700

    set up api and add submit functionality

--- public/app.js
+++ public/app.js
@@ -1,1 +1,52 @@
-console.log("This is to test!");
+const readBooksButton = document.getElementById("readBooksButton");
+
+const booksContainer = document.getElementById("booksContainer");
+
+const bookForm = document.getElementById("bookForm");
+
+const bookTitle = document.getElementById("bookTitle");
+
+const bookAuthor = document.getElementById("bookAuthor");
+
+function getBooks() {
+
+    fetch("/api/books")
+        .then((response) => response.json())
+        .then((books) => {
+
+            booksContainer.innerHTML = "";
+
+            books.forEach((book) => {
+
+                const bookElement = document.createElement("div");
+
+                bookElement.innerHTML = `
+                    <h3>${book.title}</h3>
+                    <p>Author: ${book.author}</p>
+                `;
+
+                booksContainer.appendChild(bookElement);
+            });
+
+        });
+}
+
+
+readBooksButton.addEventListener("click", getBooks);
+
+bookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const book = {
+        title: bookTitle.value,
+        author: bookAuthor.value
+    };
+
+    fetch("/api/books", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(book)
+    });
+});
--- server.js
+++ server.js
@@ -5,9 +5,35 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/api/books", (req, res) => {
+    console.log("Books API was called");
+    res.json([
+        {
+            title: "Test 1",
+            author: "Test"
+        },
+        {
+            title: "Test 2",
+            author: "Test"
+        },
+        {
+            title: "Test 3",
+            author: "Test"
+        }
+    ]);
+});
+
+app.post("/api/books", (req, res) => {
+    console.log("POST /api/books");
+    console.log(req.body);
+
+    res.status(201).json(req.body);
 });
 
 app.listen(PORT, () => {
